842146d65400521f7ab9364a8325278509db2d7d
julfy-bs <julfy.web@gmail.com>
2020-07-29 18:34:22 +0300

    29.07

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import "./styles/main.pcss";
+import "./styles/main.pcss"
 if (process.env.NODE_ENV === "development") {
-  require("file-loader!./pug/pages/home.pug");
+  require("file-loader!./pug/pages/home.pug")
 }
 
-import "./scripts/burger-menu";
-import "./scripts/slider--main";
-import "./scripts/slider--sponsors";
-import "./scripts/news-tabs";
+import "./scripts/burger-menu"
+import "./scripts/slider--main"
+import "./scripts/slider--sponsors"
+import "./scripts/news-tabs"
