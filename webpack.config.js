a04f53ffd604a8c4cad6ebead097664e91deb7cd
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2020-09-02 13:55:58 +0300

    форма gh

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = " /local/templates/pravfond/assets/";
+	const publicPath = "/prav-fund__project/";
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
