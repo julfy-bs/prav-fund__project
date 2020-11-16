6230de9d624b1f3c25e932d606b9078f44b47593
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2020-11-16 19:25:48 +0300

    Изменилась структура DOM на странице Centers Home(отображение флагов), исправлен баг с кликами на карту vmap

--- src/scripts/vmap.js
+++ src/scripts/vmap.js
@@ -8,20 +8,14 @@ $(".js-init-region-map").each(function () {
 		$map = $(".js-init-region-map-block", this),
 		$default_highlighted = $(".js-map-highlight-default"),
 		default_codes = ['ru'],
-		onRegionClickCallback = false;
+		selectedRegions = ['RU', 'AZ', 'BE', 'DK', 'GE', 'JO', 'IE', 'KZ', 'KG', 'LV', 'LT', 'CN', 'MD', 'TJ', 'SE', 'EE'],
+		states = 'az,be,dk,ge,jo,ie,kz,kg,lv,lt,cn,md,tj,se,ee';
 
 	if (russiacolor) {
 		$default_highlighted.each(function () {
 			var code = $(this).find(".flag").attr("class").replace("flag", "").trim().toLowerCase();
 			default_codes.push(code);
 		});
-		onRegionClickCallback = function (element, code, region) {
-			var $link = $(".flag." + code.toUpperCase(), $this).closest("a");
-			if ($link.length) {
-				$link.trigger("click");
-				window.location.href = $link.attr("href");
-			}
-		};
 	}
 
 	$map.vectorMap({
@@ -37,22 +31,33 @@ $(".js-init-region-map").each(function () {
 		normalizeFunction: 'polynomial',
 		selectedColor: selectedcolor,
 		multiSelectRegion: false,
-		selectedRegions: ['RU', 'AZ', 'BE', 'DK', 'GE', 'JO', 'IE', 'KZ', 'KG', 'LV', 'LT', 'CN', 'MD', 'TJ', 'SE', 'EE'],
-		onRegionClick: onRegionClickCallback,
-		showTooltip: russiacolor ? true : false,
-		onLabelShow: function (event, label, code) {
-			var $link = $(".flag." + code.toUpperCase(), $this).closest("a");
-			var $title = $(".b-coo-map__title");
-
-			if ($link.length) {
-				label.text($link.text().trim());
-				$title.text($link.text().trim());
-			} else if (code === 'ru') {
-				label.text('Российская Федерация');
-				$title.text('Российская Федерация');
+		selectedRegions: selectedRegions,
+		onRegionClick: function (event, code, region) {
+			if (states.toLowerCase().indexOf(code) <= -1) {
+				return false;
 			} else {
 				event.preventDefault();
-				$title.text('Центры правовой помощи на карте');
+				let link = $(".flag." + code.toUpperCase(), $this).closest("a");
+				if (link.length) {
+					link.trigger("click");
+					window.location.href = link.attr("href");
+				}
+			}
+		},
+		showTooltip: !!russiacolor,
+		onLabelShow: function (event, label, code) {
+			let link = $(".flag." + code.toUpperCase(), $this).closest("a");
+			let title = $(".b-coo-map__title");
+
+			if (link.length) {
+				label.text(link.text().trim());
+				title.text(link.text().trim());
+			} else if (code === 'ru') {
+				label.text('Российская Федерация');
+				title.text('Российская Федерация');
+			} else {
+				event.preventDefault();
+				title.text('Центры правовой помощи на карте');
 			}
 		}
 	});
